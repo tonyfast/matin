cb7174222621e6b9a519dc174bd679841bdc1988
System Administrator <root@ipsec-143-215-21-6.vpn.gatech.edu>
2014-03-07 16:03:27 -0500

    Fix publish dataset

--- MATLAB/api/PublishDataset.m
+++ MATLAB/api/PublishDataset.m
@@ -1,9 +1,10 @@
 function PublishDataset( YAML )
 
-system( 'git checkout gh-pages')
 t = clock;
 datapage = sprintf( '%i-%0.2i-%0.2i-%s.markdown',t(1),t(2),t(3),YAML.title);
-WriteYaml( fullfile( '_posts',horzcat(YAML.title,'.yml')), YAML );
+WriteYaml(  horzcat(YAML.title,'.yml'), YAML );
+
+system( 'git checkout gh-pages')
 fo = fopen( fullfile( '_posts', datapage ),'w' );
 fprintf( fo, '---\n');
 s = fileread( horzcat(YAML.title,'.yml') );
