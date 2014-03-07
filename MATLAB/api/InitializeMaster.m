9e97036b1cece4ef8a2e6b97e7f3814c923ea2c2
System Administrator <root@ipsec-143-215-21-17.vpn.gatech.edu>
2014-03-06 23:01:09 -0500

    Remove copied file

--- MATLAB/api/InitializeMaster.m
+++
@@ -1,9 +0,0 @@
-function InitializeMaster
-
-% soft link to the api and utility path
-addpath( genpath( 'MATLAB' ));
-
-fo = fopen( '.gitignore', 'w' );
-fseek( fo, 0,'eof');
-fprintf( fo, '\nMATLAB');
-fclose( fo );
