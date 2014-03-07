53a449310c3908fa6b83857ccc3e4af3ae5d75f6
System Administrator <root@ipsec-143-215-21-6.vpn.gatech.edu>
2014-03-07 14:22:27 -0500

    Added TestSite function that initializes a local Jekyll server

---
+++ MATLAB/api/TestSite.m
@@ -0,0 +1,7 @@
+function TestSite
+
+system('git checkout gh-pages');
+
+
+disp( '<a href="localhost:4000%s/">Test Site</a>', getfield( ReadYaml( '_config.yml'), 'baseurl') )
+system( 'jekyll serve');
