79fc1749195751c193fc0ebba50ab317d8aacf23
System Administrator <root@ipsec-143-215-21-17.vpn.gatech.edu>
2014-03-06 22:59:45 -0500

    Added to default .gitignore

--- InitializeMaster.m
+++ InitializeMaster.m
@@ -6,4 +6,6 @@ addpath( genpath( 'MATLAB' ));
 fo = fopen( '.gitignore', 'w' );
 fseek( fo, 0,'eof');
 fprintf( fo, '\nMATLAB');
+fprintf( fo, '\nInitializeMaster.m');
+fprintf( fo, '\nGitHubSetUp.m');
 fclose( fo );
