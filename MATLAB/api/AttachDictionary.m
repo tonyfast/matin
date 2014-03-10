e198e869de2c7a6a7cef141824f9dee63501eee9
System Administrator <root@ipsec-143-215-23-29.vpn.gatech.edu>
2014-03-10 18:42:16 -0400

    modified AttachDictionary

--- MATLAB/api/AttachDictionary.m
+++ MATLAB/api/AttachDictionary.m
@@ -51,6 +51,7 @@ dictpage = sprintf( '%i-%0.2i-%0.2i-%s.markdown',t(1),t(2),t(3),dictionary);
 if ~exist( fullfile( '_posts', dictpage ), 'file' )
     system('git checkout master')
     WriteYaml( horzcat(dict.name,'.yml'), dict );
+    system('git checkout gh-pages')
     movefile( horzcat(dict.name,'.yml'), fullfile( '_data',horzcat(dict.name,'.yml')) );
     fo = fopen( fullfile( '_posts', dictpage ),'w'   );
     fprintf( fo, '---\n');
